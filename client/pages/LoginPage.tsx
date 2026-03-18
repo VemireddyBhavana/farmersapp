8b9647782c295bd35bc23dd3509e1d1d07ad924f
VemireddyBhavana <bhavanavemireddy6@gmail.com>
2026-03-18 23:15:37 +0530

    Update login redirect to home page (/) and finalize Glassmorphism design

--- client/pages/LoginPage.tsx
+++ client/pages/LoginPage.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
     await signIn.authenticateWithRedirect({
       strategy: "oauth_google",
       redirectUrl: "/sso-callback",
-      redirectUrlComplete: "/dashboard",
+      redirectUrlComplete: "/",
     });
   };
 
@@ -19,7 +19,7 @@ export default function LoginPage() {
     await signIn.authenticateWithRedirect({
       strategy: "oauth_apple",
       redirectUrl: "/sso-callback",
-      redirectUrlComplete: "/dashboard",
+      redirectUrlComplete: "/",
     });
   };
 
