6cdb0f8f305a427798a4a96a1deaaf74820c785a
VemireddyBhavana <bhavanavemireddy6@gmail.com>
2026-05-14 21:47:32 +0530

    Fix ReferenceError: Badge is not defined in SoilLabLocator

--- client/pages/SoilLabLocator.tsx
+++ client/pages/SoilLabLocator.tsx
@@ -26,6 +26,7 @@ import {
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Badge } from "@/components/ui/badge";
 import { useLanguage } from "@/lib/LanguageContext";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
